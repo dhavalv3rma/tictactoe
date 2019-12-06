dad82e7d0d368f4b86acbfac0e1b5d94117f938f
dhavalv3rma <dhavalverma@live.in>
2019-12-07 00:07:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,28 +29,35 @@ function check() {
 
     /*  HORIZONTAL  */
     if (one === two && two === three && three != "" && two != "" && one != "") {
+        $("#1, #2, #3").css('background', '#68e56c');
         wins(two);
 
     } else if (four === five && five === six && four != "" && five != "" && six != "") {
+        $("#4, #5, #6").css('background', '#68e56c');
         wins(five);
     } else if (seven === eight && eight === nine && seven != "" && eight != "" && nine != "") {
+        $("#7, #8, #9").css('background', '#68e56c');
         wins(eight);
-
     }
 
     /*  VERTICAL  */
     else if (one === four && four === seven && one != "" && four != "" && seven != "") {
+        $("#1, #4, #7").css('background', '#68e56c');
         wins(four);
     } else if (two === five && five === eight && two != "" && five != "" && eight != "") {
+        $("#2, #5, #8").css('background', '#68e56c');
         wins(five);
     } else if (three === six && six === nine && three != "" && six != "" && nine != "") {
+        $("#3, #6, #9").css('background', '#68e56c');
         wins(six);
     }
 
     /*  DIAGONAL  */
     else if (one === five && five === nine && one != "" && five != "" && nine != "") {
+        $("#1, #5, #9").css('background', '#68e56c');
         wins(one);
     } else if (three === five && five === seven && three != "" && five != "" && seven != "") {
+        $("#3, #5, #7").css('background', '#68e56c');
         wins(three);
     } else {
 
@@ -70,18 +77,10 @@ function check() {
 
     /*  WIN CONDITION   */
     function wins(a) {
-        /*        if (a == null || a == "" || a == undefined) {
-                    $("#result").text('');
-                } else {
-                    $("#result").text(a + ' Wins');
-                }*/
-
         $("#result").text(a + ' Wins');
         $('.parts').prop("disabled", true);
-
     }
 }
-
 
 $("#reset").click(function () {
     location.reload();
